4ab526172807d3b09508744848c202a33f5fb172
M03ED <50927468+M03ED@users.noreply.github.com>
2025-02-19 21:44:15 +0330

    fix: format dashboard directory

--- app/dashboard/__init__.py
+++ app/dashboard/__init__.py
@@ -7,37 +7,38 @@ from config import DEBUG, VITE_BASE_API, DASHBOARD_PATH, UVICORN_PORT
 from fastapi.staticfiles import StaticFiles
 
 base_dir = Path(__file__).parent
-build_dir = base_dir / 'build'
-statics_dir = build_dir / 'statics'
+build_dir = base_dir / "build"
+statics_dir = build_dir / "statics"
 
 
 def build_api_interface():
     proc = subprocess.Popen(
-        ['pnpm', 'run', 'wait-port-gen-api'],
-        env={**os.environ, 'UVICORN_PORT': str(UVICORN_PORT)},
+        ["pnpm", "run", "wait-port-gen-api"],
+        env={**os.environ, "UVICORN_PORT": str(UVICORN_PORT)},
         cwd=base_dir,
-        stdout=subprocess.DEVNULL
+        stdout=subprocess.DEVNULL,
     )
+
 
 def build():
     proc = subprocess.Popen(
-        ['pnpm', 'run', 'build', '--outDir', build_dir, '--assetsDir', 'statics'],
-        env={**os.environ, 'VITE_BASE_API': VITE_BASE_API},
-        cwd=base_dir
+        ["pnpm", "run", "build", "--outDir", build_dir, "--assetsDir", "statics"],
+        env={**os.environ, "VITE_BASE_API": VITE_BASE_API},
+        cwd=base_dir,
     )
     proc.wait()
-    with open(build_dir / 'index.html', 'r') as file:
+    with open(build_dir / "index.html", "r") as file:
         html = file.read()
-    with open(build_dir / '404.html', 'w') as file:
+    with open(build_dir / "404.html", "w") as file:
         file.write(html)
 
 
 def run_dev():
     build_api_interface()
     proc = subprocess.Popen(
-        ['pnpm', 'run', 'dev', '--base', os.path.join(DASHBOARD_PATH, '')],
-        env={**os.environ, 'VITE_BASE_API': VITE_BASE_API, 'DEBUG': 'false'},
-        cwd=base_dir
+        ["pnpm", "run", "dev", "--base", os.path.join(DASHBOARD_PATH, "")],
+        env={**os.environ, "VITE_BASE_API": VITE_BASE_API, "DEBUG": "false"},
+        cwd=base_dir,
     )
 
     atexit.register(proc.terminate)
@@ -47,16 +48,8 @@ def run_build():
     if not build_dir.is_dir():
         build()
 
-    app.mount(
-        DASHBOARD_PATH,
-        StaticFiles(directory=build_dir, html=True),
-        name="dashboard"
-    )
-    app.mount(
-        '/statics/',
-        StaticFiles(directory=statics_dir, html=True),
-        name="statics"
-    )
+    app.mount(DASHBOARD_PATH, StaticFiles(directory=build_dir, html=True), name="dashboard")
+    app.mount("/statics/", StaticFiles(directory=statics_dir, html=True), name="statics")
 
 
 @app.on_event("startup")
