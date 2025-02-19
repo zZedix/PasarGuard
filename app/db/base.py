fe177d1d2f2168c6f2e2976fd3b9bc0741c7c7ce
M03ED <50927468+M03ED@users.noreply.github.com>
2025-02-19 21:44:37 +0330

    fix: format db directory

--- app/db/base.py
+++ app/db/base.py
@@ -6,20 +6,17 @@ from config import (
     SQLIALCHEMY_MAX_OVERFLOW,
 )
 
-IS_SQLITE = SQLALCHEMY_DATABASE_URL.startswith('sqlite')
+IS_SQLITE = SQLALCHEMY_DATABASE_URL.startswith("sqlite")
 
 if IS_SQLITE:
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL,
-        connect_args={"check_same_thread": False}
-    )
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 else:
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL,
         pool_size=SQLALCHEMY_POOL_SIZE,
         max_overflow=SQLIALCHEMY_MAX_OVERFLOW,
         pool_recycle=3600,
-        pool_timeout=10
+        pool_timeout=10,
     )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
