e06fb6310fe1e1d1d0680add0ffa2db7253102db
Mohammad <immohammad20000@protonmail.com>
2025-06-11 20:49:19 +0330

    fix: some issue related to userTemplate

--- app/models/user_template.py
+++ app/models/user_template.py
@@ -31,7 +31,7 @@ class UserTemplate(BaseModel):
     status: UserStatusCreate | None = None
     reset_usages: bool | None = None
     on_hold_timeout: int | None = None
-    data_limit_reset_strategy: UserDataLimitResetStrategy | None = None
+    data_limit_reset_strategy: UserDataLimitResetStrategy = UserDataLimitResetStrategy.no_reset
     is_disabled: bool | None = None
 
 
