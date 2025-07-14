ca98d3226fa848de5a8a7bfc69a3018293526ed7
M03ED <50927468+M03ED@users.noreply.github.com>
2025-07-14 14:05:53 +0330

    fix: better performance in calling functions

--- app/operation/system.py
+++ app/operation/system.py
@@ -42,13 +42,13 @@ class SystemOperation(BaseOperation):
             limited_users,
             online_users,
         ) = await asyncio.gather(
-            get_users_count(db, admin_id=admin_id),
-            get_users_count(db, status=UserStatus.active, admin_id=admin_id),
-            get_users_count(db, status=UserStatus.disabled, admin_id=admin_id),
-            get_users_count(db, status=UserStatus.on_hold, admin_id=admin_id),
-            get_users_count(db, status=UserStatus.expired, admin_id=admin_id),
-            get_users_count(db, status=UserStatus.limited, admin_id=admin_id),
-            count_online_users(db, timedelta(minutes=2), admin_id=admin_id),
+            get_users_count(db, None, admin_id),
+            get_users_count(db, UserStatus.active, admin_id),
+            get_users_count(db, UserStatus.disabled, admin_id),
+            get_users_count(db, UserStatus.on_hold, admin_id),
+            get_users_count(db, UserStatus.expired, admin_id),
+            get_users_count(db, UserStatus.limited, admin_id),
+            count_online_users(db, timedelta(minutes=2), admin_id),
         )
 
         return SystemStats(
